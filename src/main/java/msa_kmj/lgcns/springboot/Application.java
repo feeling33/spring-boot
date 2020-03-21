8d293d34892e52103feae7b25740453800215d96
feeling33 <feeling33@gmail.com>
2020-03-22 01:33:49 +0900

    first commit

--- src/main/java/msa_kmj/lgcns/springboot/Application.java
+++ src/main/java/msa_kmj/lgcns/springboot/Application.java
@@ -2,8 +2,6 @@ package msa_kmj.lgcns.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
 
 @SpringBootApplication
 public class Application {
--- src/test/java/msa_kmj/lgcns/springboot/service/BookServiceTest.java
+++ src/test/java/msa_kmj/lgcns/springboot/service/BookServiceTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import msa_kmj.lgcns.springboot.domain.BookRepository;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 public class BookServiceTest {
