a5cf1778234b7b89cb06543c1e3416b08767651d
feeling33 <feeling33@gmail.com>
2020-03-22 00:34:49 +0900

    first commit

--- src/main/java/msa_kmj/lgcns/springboot/Application.java
+++ src/main/java/msa_kmj/lgcns/springboot/Application.java
@@ -2,8 +2,11 @@ package msa_kmj.lgcns.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"msa_kmj.lgcns.springboot.api"})
 public class Application {
 
 	public static void main(String[] args) {
