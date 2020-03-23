751be3ad42797456fe0f0fa96c74db89c72cfc44
feeling33 <feeling33@gmail.com>
2020-03-23 21:01:18 +0900

    first commit

--- src/main/java/msa_kmj/lgcns/springboot/api/BookController.java
+++ src/main/java/msa_kmj/lgcns/springboot/api/BookController.java
@@ -11,13 +11,15 @@ import msa_kmj.lgcns.springboot.service.BookService;
 import org.springframework.web.bind.annotation.PathVariable;  
 
 @RestController
-@RequestMapping(value = "/books")
+//@RequestMapping(value = "/books")
+@RequestMapping(value = "/{bookId}") //==> "/{bookId}"
 public class BookController {
 	
 	@Autowired
 	BookService bookService;
 	
 	@GetMapping("/{bookId}")
+	//public ResponseEntity findById(@PathVariable Long bookId){
 	public ResponseEntity findById(@PathVariable Long bookId){
 		Book book = bookService.findById(bookId)
 				.orElseThrow(() -> new RuntimeException("Not found: " + bookId));
